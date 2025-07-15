7bcd93a457662b1ca5250b010ba349202cddf124
MukulShekhar <111970240+MukulShekhar@users.noreply.github.com>
2025-07-15 16:38:56 +0530

    Time: 94 ms (11.56%) | Memory: 58.5 MB (74.86%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,22 +1,31 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> res=new ArrayList<>();
-        if(nums1.length==0 || nums2.length==0 || k==0){
-            return res;
-        }
-        PriorityQueue<int[]> pq=new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
-        for(int i=0;i<Math.min(nums1.length,k);i++){
-            pq.offer(new int[]{nums1[i]+nums2[0],i,0});
-        }
+        int n=nums1.length;
+        int m=nums2.length;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> (a[0]-b[0]));
+        HashSet<String> vis=new HashSet<>();
+        List<List<Integer>> ans=new ArrayList<>();
+
+        pq.add(new int[]{nums1[0]+nums2[0],0,0});
+        vis.add("0,0");
+
         while(k-->0 && !pq.isEmpty()){
             int[] curr=pq.poll();
+            int sum=curr[0];
             int i=curr[1];
             int j=curr[2];
-            res.add(Arrays.asList(nums1[i],nums2[j]));
-            if(j+1<nums2.length){
-                pq.offer(new int[]{nums1[i]+nums2[j+1],i,j+1});
+            ans.add(Arrays.asList(nums1[i],nums2[j]));
+
+            if(i+1<n  && !vis.contains((i+1)+","+j)){
+                pq.add(new int[]{nums1[i+1]+nums2[j],i+1,j});
+                vis.add((i+1)+","+j);
+            }
+
+            if(j+1<m  && !vis.contains(i+","+(j+1))){
+                pq.add(new int[]{nums1[i]+nums2[j+1],i,j+1});
+                vis.add(i+","+(j+1));
             }
         }
-        return res;
+        return ans;
     }
 }

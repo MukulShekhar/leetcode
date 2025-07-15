79c494f64392079afb19da9c21f0ae363a92201b
MukulShekhar <111970240+MukulShekhar@users.noreply.github.com>
2025-07-15 12:00:55 +0530

    Time: 1 ms (99.12%) | Memory: 42.1 MB (62.25%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String word) {
+        int n=word.length();
+        if(n<3){
+            return false;
+        }
+        int vow=0;
+        int cons=0;
+        for(char c:word.toCharArray()){
+            if(Character.isLetter(c)){
+                if("aeiouAEIOU".indexOf(c)!=-1){
+                    vow++;
+                }else{
+                    cons++;
+                }
+            }
+            else if(!Character.isDigit(c)){
+                return false;
+            }
+        }
+        return vow>=1 && cons>=1 ;
+    }
+}

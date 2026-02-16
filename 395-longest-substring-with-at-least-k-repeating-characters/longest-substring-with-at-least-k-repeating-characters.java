8c672854fe9628deffdd3812939394162ae37408
MukulShekhar <111970240+MukulShekhar@users.noreply.github.com>
2026-02-16 11:10:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (92.16%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,20 +1,18 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        return helper(s.toCharArray(),0,s.length(),k);
+        return h(s.toCharArray(),0,s.length(),k);
     }
-    int helper(char[] ch,int start,int end,int k){
+    private int h(char[] ch,int start,int end,int k){
         if(end-start<k) return 0;
-        int[] count=new int[26];
+        int[] co=new int[26];
+        for(int i=start;i<end;i++) co[ch[i]-'a']++;
         for(int i=start;i<end;i++){
-            count[ch[i]-'a']++;
-        }
-        for(int i=start;i<end;i++){
-            if(count[ch[i]-'a']<k){
+            if(co[ch[i]-'a']<k){
                 int j=i+1;
-                while(j<end && count[ch[j]-'a']<k){
+                while(j<end && co[ch[j]-'a']<k){
                     j++;
                 }
-                return Math.max(helper(ch,start,i,k),helper(ch,j,end,k));
+                return Math.max(h(ch,start,i,k),h(ch,j,end,k));
             }
         }
         return end-start;

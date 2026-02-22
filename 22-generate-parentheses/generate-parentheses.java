166d58c1f8b36455b422a686a80b825010376cf0
MukulShekhar <111970240+MukulShekhar@users.noreply.github.com>
2026-02-22 18:16:43 +0530

    Time: 2 ms (68.20%) | Memory: 45.1 MB (22.48%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<List<String>> dp=new ArrayList<>();
-        dp.add(Arrays.asList(""));
-        for(int i=1;i<=n;i++){
-            List<String> curr=new ArrayList<>();
-            for(int j=0;j<i;j++){
-                for(String left:dp.get(j)){
-                    for(String right:dp.get(i-1-j)){
-                        curr.add("("+left+")"+right);
-                    }
-                }
-            }
-            dp.add(curr);
+        List<String> res=new ArrayList<>();
+        dfs("",0,0,res,n);
+        return res;
+    }
+    private void dfs(String curr,int left,int right,List<String> res,int n){
+        if(left==n && right==n){
+            res.add(curr);
+            return;
         }
-        return dp.get(n);
+        if(left<n){
+            dfs(curr + "(",left+1,right,res,n);
+        }
+        if(left>right){
+            dfs(curr + ")",left,right+1,res,n);
+        }
     }
 }

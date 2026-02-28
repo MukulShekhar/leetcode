24db310a351e9053fe4d35a50fa109f09902eef8
MukulShekhar <111970240+MukulShekhar@users.noreply.github.com>
2026-02-28 12:51:10 +0530

    Time: 2 ms (91.23%) | Memory: 47.7 MB (94.86%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,26 +1,26 @@
 class Solution {
     public void dfs(int[][] graph,int node,List<List<Integer>> res,List<Integer> path,boolean[] vis,int dest){
         if(vis[node]){
-            return;
-        }
-        vis[node]=false;
-        path.add(node);
-        if(node==dest){
-            List<Integer> temp=new ArrayList<>();
-            for(int i:path){
-                temp.add(i);
-            }
-            res.add(temp);
-        }
-        for(int i:graph[node]){
-            dfs(graph,i,res,path,vis,dest);
-        }
-
-        path.remove(path.size()-1);
+        return;
     }
+    vis[node]=false;
+    path.add(node);
+    if(node==dest){
+        List<Integer> temp=new ArrayList<>();
+        for(int i:path){
+            temp.add(i);
+        }
+        res.add(temp);
+    }
+    for(int i:graph[node]){
+        dfs(graph,i,res,path,vis,dest);
+    }
+    path.remove(path.size()-1);
+}
+
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         int n=graph.length;
-        List<List<Integer>> res=new ArrayList<>();
+        List<List<Integer>> res=new ArrayList<List<Integer>>();
         List<Integer> path=new ArrayList<>();
         boolean[] vis=new boolean[n];
         dfs(graph,0,res,path,vis,n-1);

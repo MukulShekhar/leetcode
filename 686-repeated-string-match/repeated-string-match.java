1606f71785e3f813e5d5cc8edfff362dc8609f0c
MukulShekhar <111970240+MukulShekhar@users.noreply.github.com>
2026-03-28 12:14:52 +0530

    Time: 261 ms (64.78%) | Memory: 43.3 MB (97.17%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,7 +1,7 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         StringBuilder sb=new StringBuilder(a);
-        int count =1;
+        int count=1;
         while(sb.length()<b.length()){
             sb.append(a);
             count++;
